fa9cb84b069b2923a71bf570b2f97c37dd55de36
Patricio Perez <patricio.perez@ceinf.cl>
2013-05-29 21:41:37 -0400

    * Implementar logica de BST que hicimos con el seba

--- bst.py
+++ bst.py
@@ -14,17 +14,57 @@ class BST(object):
         '''
         Constructor
         '''
-        pass
+        self.raiz = {}  #NULL
+        
+        # Si pasa un iterable, llenamos el arbol con el
+        if iterable is not None:
+            for item in iterable:
+                self.add(item)
+            
     
-    def add(self, item):
+    def add(self, dato, nodo=None):
         '''
         '''
-        pass
+        if nodo is None:
+            nodo = self.raiz
+        
+        if nodo == {}:
+            nodo['izq'] = {}
+            nodo['der'] = {}
+            nodo['key'] = dato
+        else:
+            if dato < nodo['key']:
+                self.add(dato, nodo['izq'])
+            else:
+                self.add(dato, nodo['der'])
     
-    def inorder(self):
+    def inorder(self, nodo = None, elementos = None):
         '''
         '''
-        pass
+        
+        if nodo is None: # Primera ejecucion
+            elementos = []
+            nodo = self.raiz
+        
+        if nodo['izq'] != {}:
+            self.inorder(nodo['izq'], elementos)
+        
+        elementos.append(nodo['key'])
+        
+        if nodo['der'] != {}:
+            self.inorder(nodo['der'], elementos)
+        
+        return elementos
+            
+    def mostrar(self, nodo=None):
+        if nodo is None:
+            nodo = self.raiz
+
+        if nodo != {}:
+            self.mostrar(nodo['izq'])
+            print nodo['key']
+            #print "Dato:", nodo['key'], "Factor equilibrio", nodo['factor_equilibrio']
+            self.mostrar(nodo['der'])
     
     def plot(self):
         '''
